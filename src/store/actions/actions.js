53cf3af2492dd9fad857d6c908c96356d836400d
Keith McCall <keithmccall@Keiths-MacBook-Pro.local>
2018-06-03 14:50:09 -0400

    configuring

--- src/store/actions/actions.js
+++ src/store/actions/actions.js
@@ -4,7 +4,7 @@ import axios from "axios";
 export const fetchPresidents = fetchType => dispatch => {
   console.log(fetchType)
   let path = fetchType == undefined ? "" : fetchType
-  axios(`http://presidentsapi.azurewebsites.net/api/presidents`).then(results =>
+  axios(`https://presidentsapi.azurewebsites.net/api/presidents`).then(results =>
     dispatch({
       type: FETCH_PRESIDENTS,
       payload: results.data
